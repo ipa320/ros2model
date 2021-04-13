10840c8c14fc765756008ddbbd49cab4456ece59
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-04-13 10:25:42 +0200

    fix import

--- src/ros_model_generator/rosgraph_interface.py
+++ src/ros_model_generator/rosgraph_interface.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import ros_graph_parser.core_class as model
+from ros_graph_parser.core_class import *
 
 
 class JavaInterface(Interface):
