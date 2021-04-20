92580c1291c30bc0aebd3d262b644b3156cb944b
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-04-20 15:46:37 +0200

    added example for generating rossystem from dict

--- src/ros_model_generator/rossystem_generator.py
+++ src/ros_model_generator/rossystem_generator.py
@@ -53,29 +53,35 @@ class RosSystemModelGenerator(object):
     for name in components:
       component = system_model.Component(name)
 
-      publishers = components[name]['publishers']
-      for pub, pub_type in publishers.items():
-        component.publishers.add(system_model.RosInterface(pub, pub_type))
+      if 'publishers' in components[name]:
+        publishers = components[name]['publishers']
+        for pub, pub_type in publishers.items():
+          component.publishers.add(system_model.RosInterface(pub, pub_type))
 
-      subscribers = components[name]['publishers']
-      for sub, sub_type in subscribers.items():
-        component.subscribers.add(system_model.RosInterface(sub, sub_type))
+      if 'subscribers' in components[name]:
+        subscribers = components[name]['subscribers']
+        for sub, sub_type in subscribers.items():
+          component.subscribers.add(system_model.RosInterface(sub, sub_type))
 
-      service_servers = components[name]['service_servers']
-      for serv, serv_type in service_servers.items():
-        component.service_servers.add(system_model.RosInterface(serv, serv_type))
+      if 'service_servers' in components[name]:
+        service_servers = components[name]['service_servers']
+        for serv, serv_type in service_servers.items():
+          component.service_servers.add(system_model.RosInterface(serv, serv_type))
 
-      service_clients = components[name]['service_clients']
-      for serv, serv_type in service_clients.items():
-        component.service_clients.add(system_model.RosInterface(serv, serv_type))
+      if 'service_clients' in components[name]:
+        service_clients = components[name]['service_clients']
+        for serv, serv_type in service_clients.items():
+          component.service_clients.add(system_model.RosInterface(serv, serv_type))
 
-      action_clients = components[name]['action_clients']
-      for action, action_type in action_clients.items():
-        component.action_clients.add(system_model.RosInterface(action, action_type))
+      if 'action_clients' in components[name]:
+        action_clients = components[name]['action_clients']
+        for action, action_type in action_clients.items():
+          component.action_clients.add(system_model.RosInterface(action, action_type))
 
-      action_servers = components[name]['action_servers']
-      for action, action_type in action_servers.items():
-        component.action_servers.add(system_model.RosInterface(action, action_type))
+      if 'action_servers' in components[name]:
+        action_servers = components[name]['action_servers']
+        for action, action_type in action_servers.items():
+          component.action_servers.add(system_model.RosInterface(action, action_type))
 
       self.addComponent(component)
 
@@ -84,8 +90,16 @@ class RosSystemModelGenerator(object):
 
 if __name__ == "__main__":
   generator = RosSystemModelGenerator()
+  components = {'/gazebo': {'publishers': {'/gazebo/link_states': 'gazebo_msgs/LinkStates',
+                                           '/gazebo/model_states': 'gazebo_msgs/ModelStates'},
+                            'subscribers': {'/gazebo/set_link_state': 'gazebo_msgs/LinkState',
+                                            '/gazebo/set_model_state': 'gazebo_msgs/ModelState'},
+                            'service_servers': {'/gazebo/spawn_sdf_model': 'gazebo_msgs/SpawnModel',
+                                                '/gazebo/spawn_urdf_model': 'gazebo_msgs/SpawnModel'}},
+              '/fibonacci': {'action_servers': {'/fibonacci': 'actionlib_tutorials/Fibonacci'}}}
   try:
-    print(generator.dump_java_ros_system_model("/tmp/test").dump())
+    # print(generator.dump_ros_system_model("/tmp/test").dump())
+    print(generator.create_ros_system_model_list(components)[1])
   except Exception as e:
     print(e.args)
 
