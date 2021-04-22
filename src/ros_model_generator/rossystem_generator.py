ef6e67d10006655b90f3bcbd372c992be261aef9
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-04-22 16:58:52 +0200

    add parameters from dict to system model

--- src/ros_model_generator/rossystem_generator.py
+++ src/ros_model_generator/rossystem_generator.py
@@ -16,12 +16,11 @@
 
 import pprint
 from pyparsing import *
-import ros_metamodels.rossystem_metamodel_core as system_model
-import ros_metamodels.ros_metamodel_core as model
+import ros_metamodels.rossystem_metamodel_core as model
 
 class RosSystemModelGenerator(object):
   def __init__(self,name=""):
-    self.system = system_model.RosSystem(name);
+    self.system = model.RosSystem(name);
 
   def setSystemName(self, name):
     self.system.name = name;
@@ -30,7 +29,7 @@ class RosSystemModelGenerator(object):
     self.system.params.add(model.Parameter(name, value, type(value)))
 
   def addComponent(self, name):
-    self.system.components.add(system_model.Component(name))
+    self.system.components.add(model.Component(name))
 
   def addComponent(self, component):
     self.system.components.add(component)
@@ -51,38 +50,50 @@ class RosSystemModelGenerator(object):
 
   def create_ros_system_model_list(self, components):
     for name in components:
-      component = system_model.Component(name)
+      component = model.Component(name)
+
+      if 'parameters' in components[name]:
+        parameters = components[name]['parameters']
+        for name, value in parameters.items():
+          component.params.add(model.RosParameter(name, value))
 
       if 'publishers' in components[name]:
         publishers = components[name]['publishers']
         for pub, pub_type in publishers.items():
-          component.publishers.add(system_model.RosInterface(pub, pub_type))
+          component.publishers.add(model.RosInterface(pub, pub_type))
 
       if 'subscribers' in components[name]:
         subscribers = components[name]['subscribers']
         for sub, sub_type in subscribers.items():
-          component.subscribers.add(system_model.RosInterface(sub, sub_type))
+          component.subscribers.add(model.RosInterface(sub, sub_type))
 
       if 'service_servers' in components[name]:
         service_servers = components[name]['service_servers']
         for serv, serv_type in service_servers.items():
-          component.service_servers.add(system_model.RosInterface(serv, serv_type))
+          component.service_servers.add(model.RosInterface(serv, serv_type))
 
       if 'service_clients' in components[name]:
         service_clients = components[name]['service_clients']
         for serv, serv_type in service_clients.items():
-          component.service_clients.add(system_model.RosInterface(serv, serv_type))
+          component.service_clients.add(model.RosInterface(serv, serv_type))
 
       if 'action_clients' in components[name]:
         action_clients = components[name]['action_clients']
         for action, action_type in action_clients.items():
-          component.action_clients.add(system_model.RosInterface(action, action_type))
+          component.action_clients.add(model.RosInterface(action, action_type))
 
       if 'action_servers' in components[name]:
         action_servers = components[name]['action_servers']
         for action, action_type in action_servers.items():
-          component.action_servers.add(system_model.RosInterface(action, action_type))
+          component.action_servers.add(model.RosInterface(action, action_type))
 
+      self.addComponent(component)
+
+    if 'parameters_node' in components:
+      component = model.Component('parameters_node')
+      parameters = components['parameters_node']['parameters']
+      for name, value in parameters.items():
+        component.params.add(model.RosParameter(name, value))
       self.addComponent(component)
 
     return self.create_ros_system_model()
