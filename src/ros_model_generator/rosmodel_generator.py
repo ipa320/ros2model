b84b13401b2e02ff72b07b7064a1c1fb97082e70
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-07-09 13:42:34 +0200

    generate ros model from components data struct

--- src/ros_model_generator/rosmodel_generator.py
+++ src/ros_model_generator/rosmodel_generator.py
@@ -33,9 +33,60 @@ class RosModelGenerator(object):
     with open(ros_model_file, 'w') as outfile:
       outfile.write(ros_model_str)
 
+  def generate_ros_model_list(self, components, ros_model_file):
+    sucess, ros_model_str = self.create_ros_model_list(components)
+    with open(ros_model_file, 'w') as outfile:
+      outfile.write(ros_model_str)
+
   def create_ros_model(self):
     ros_model_str = self.ros_model.dump_xtext_model()
     return True, ros_model_str
+
+  def create_ros_model_list(self, components):
+    for name in components:
+      if name == 'global_parameters':
+        continue
+      node = model.Node(name)
+
+      if 'parameters' in components[name]:
+        parameters = components[name]['parameters']
+        for param_name, param in parameters.items():
+          node.add_parameter(param_name, None, None, param[0])
+
+      if 'publishers' in components[name]:
+        publishers = components[name]['publishers']
+        for pub, pub_type in publishers.items():
+          node.add_publisher(pub, pub_type)
+
+      if 'subscribers' in components[name]:
+        subscribers = components[name]['subscribers']
+        for sub, sub_type in subscribers.items():
+          node.add_subscriber(sub, sub_type)
+
+      if 'service_servers' in components[name]:
+        service_servers = components[name]['service_servers']
+        for serv, serv_type in service_servers.items():
+          node.add_service_server(serv, serv_type)
+
+      if 'service_clients' in components[name]:
+        service_clients = components[name]['service_clients']
+        for serv, serv_type in service_clients.items():
+          node.add_service_client(serv, serv_type)
+
+      if 'action_clients' in components[name]:
+        action_clients = components[name]['action_clients']
+        for action, action_type in action_clients.items():
+          node.add_action_client(action, action_type)
+
+      if 'action_servers' in components[name]:
+        action_servers = components[name]['action_servers']
+        for action, action_type in action_servers.items():
+          node.add_action_server(action, action_type)
+
+      self.create_model_from_node('my_ros_pkg',"test",node)
+
+    return self.create_ros_model()
+
 
 if __name__ == "__main__":
   generator = RosModelGenerator()
